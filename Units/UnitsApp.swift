ba3f83cec78fbb67e12d1f2566ea9509feef7def
jezsdaniel <jadendaniel7@gmail.com>
2023-07-19 00:22:46 -0400

    Initial Commit

---
+++ Units/UnitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsApp.swift
+//  Units
+//
+//  Created by J. Daniel F. Ruano on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
